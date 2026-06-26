569c0377517238207f5c5fcb05f04d138aae9393
HarshPratapSingh1 <harshpratap715@gmail.com>
2026-06-26 11:34:43 +0530

    fixed git ignore

---
+++ Introductory problems/weirdAlgo.java
@@ -0,0 +1,23 @@
+package cses;
+import java.util.*;
+public class weirdAlgo {
+	public static void main(String[]args) {
+		Scanner scan=new Scanner(System.in);
+		long n=scan.nextLong();
+		ArrayList<Long>l=new ArrayList<>();
+		l.add(n);
+		while(n!=1) {
+			if(n%2==0) {
+				n/=2;
+			}
+			else if(n%2!=0) {
+				n*=3;
+				n++;
+			}
+			l.add(n);
+		}
+		for(Long it:l) {
+			System.out.print(it+" ");
+		}
+	}
+}
