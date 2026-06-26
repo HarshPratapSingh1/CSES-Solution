ee5fac0a0b3de5ba6a1e6aca7c897b1026c085d6
HarshPratapSingh1 <harshpratap715@gmail.com>
2026-06-26 08:43:44 +0530

    +1

---
+++ dynamic programming/IncreasingSubsequence.java
@@ -0,0 +1,147 @@
+import java.util.*;
+import java.io.*;
+
+public class IncreasingSubsequence {
+    static FastIO scan;
+    static final int MOD = 1_000_000_007;
+    static final long LINF = (long) 1e18;
+
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("input.txt"));
+        // System.setOut(new PrintStream("output.txt"));
+        scan = new FastIO();
+        int t = 1;
+        // t = scan.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        scan.close();
+    }
+
+    static int n;
+    static long arr[];
+
+    static void solve() throws IOException {
+        n = scan.nextInt();
+        arr = new long[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = scan.nextLong();
+
+        ArrayList<Long> cnt = new ArrayList<>();
+        cnt.add(0L);
+
+        for (int i = 0; i < n; i++) {
+            long val = arr[i];
+
+            int idx = bs(cnt, val);
+            if (idx < cnt.size()) {
+                cnt.set(idx, val);
+            } else {
+                cnt.add(val);
+            }
+        }
+        System.out.println(cnt.size() - 1);
+
+    }
+
+    public static int bs(ArrayList<Long> li, long val) {
+        int l = 0, r = li.size() - 1;
+        int ans = li.size();
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (li.get(mid) >= val) {
+                ans = mid;
+                r = mid - 1;
+            } else
+                l = mid + 1;
+        }
+        return ans;
+    }
+
+    // ---------------------- FAST I/O ----------------------
+    static class FastIO {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+        private final PrintWriter out = new PrintWriter(System.out);
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                ptr = 0;
+                len = in.read(buffer);
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        public String next() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return null;
+            StringBuilder sb = new StringBuilder();
+            while (c > 32) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return 0;
+            int s = 1;
+            if (c == '-') {
+                s = -1;
+                c = readByte();
+            }
+            int v = 0;
+            while (c > 32) {
+                v = v * 10 + (c - '0');
+                c = readByte();
+            }
+            return v * s;
+        }
+
+        public long nextLong() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return 0;
+            int s = 1;
+            if (c == '-') {
+                s = -1;
+                c = readByte();
+            }
+            long v = 0;
+            while (c > 32) {
+                v = v * 10 + (c - '0');
+                c = readByte();
+            }
+            return v * s;
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public void pn(Object o) {
+            out.println(o);
+        }
+
+        public void p(Object o) {
+            out.print(o);
+        }
+
+        public void close() {
+            out.close();
+        }
+    }
+}
