40b6f4b365f205c0aa8dffddf74ce23ed16b62f8
HarshPratapSingh1 <harshpratap715@gmail.com>
2026-08-17 01:17:31 +0530

    +1

---
+++ mathematics/ChristmasParty.java
@@ -0,0 +1,125 @@
+import java.util.*;
+import java.io.*;
+
+public class ChristmasParty {
+    static FastIO scan;
+    static final int MOD = 1_000_000_007;
+    static final long LINF = (long) 1e18;
+
+    public static void main(String[] args) throws Exception {
+        // System.setIn(new FileInputStream("input.txt"));
+        // System.setOut(new PrintStream("output.txt"));
+        scan = new FastIO();
+        int t = 1;
+        arr = new long[(int) 1e6 + 1];
+        precompute();
+        // t = scan.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        scan.close();
+    }
+
+    static long arr[];
+
+    public static void precompute() {
+        arr[1] = 0;
+        arr[2] = 1;
+
+        for (int i = 3; i <= 1e6; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % MOD;
+            arr[i] = arr[i] * (i - 1) % MOD;
+        }
+    }
+
+    static void solve() throws IOException {
+        long n = scan.nextLong();
+        System.out.println(arr[(int) n]);
+    }
+
+    // ---------------------- FAST I/O ----------------------
+    static class FastIO {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+        private final PrintWriter out = new PrintWriter(System.out);
+
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                ptr = 0;
+                len = in.read(buffer);
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        public String next() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return null;
+            StringBuilder sb = new StringBuilder();
+            while (c > 32) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return 0;
+            int s = 1;
+            if (c == '-') {
+                s = -1;
+                c = readByte();
+            }
+            int v = 0;
+            while (c > 32) {
+                v = v * 10 + (c - '0');
+                c = readByte();
+            }
+            return v * s;
+        }
+
+        public long nextLong() throws IOException {
+            int c = readByte();
+            while (c >= 0 && c <= 32)
+                c = readByte();
+            if (c == -1)
+                return 0;
+            int s = 1;
+            if (c == '-') {
+                s = -1;
+                c = readByte();
+            }
+            long v = 0;
+            while (c > 32) {
+                v = v * 10 + (c - '0');
+                c = readByte();
+            }
+            return v * s;
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public void pn(Object o) {
+            out.println(o);
+        }
+
+        public void p(Object o) {
+            out.print(o);
+        }
+
+        public void close() {
+            out.close();
+        }
+    }
+}
